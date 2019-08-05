c747eb3f49a9586b44f9f899d0b8f3d41de766f4
Maksim Kostromin <daggerok@gmail.com>
2019-08-05 19:52:03 +0300

    Add maven infrastructure project together with data-structures module.

---
+++ data-structures/src/main/java/com/github/vmalaya/sigmasoftware/internship/datastructures/Verify.java
@@ -0,0 +1,7 @@
+package com.github.vmalaya.sigmasoftware.internship.datastructures;
+
+public class Verify {
+    public static void main(String[] args) {
+        System.out.println("ololo");
+    }
+}
---
+++ data-structures/src/test/groovy/com/github/vmalaya/sigmasoftware/internship/datastructures/VerifyTest.groovy
@@ -0,0 +1,10 @@
+package com.github.vmalaya.sigmasoftware.internship.datastructures
+
+import spock.lang.Specification
+
+class VerifyTest extends Specification {
+    def 'hey'() {
+        expect:
+            1 == 1
+    }
+}
