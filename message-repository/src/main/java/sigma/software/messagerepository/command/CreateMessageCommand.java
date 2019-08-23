c95907b89dd04f94c8b833913c8aec6af54bc87f
Valentyna Mala <valentinkamalaya@gmail.com>
2019-08-23 12:09:22 +0300

    DS-1: Add receiving messages. #1

--- message-repository/src/main/java/sigma/software/messagerepository/command/CreateMessageCommand.java
+++ message-repository/src/main/java/sigma/software/messagerepository/command/CreateMessageCommand.java
@@ -5,21 +5,15 @@ import java.util.UUID;
 public class CreateMessageCommand {
 
     private final UUID id;
-    private final UUID idFriend;
     private final String text;
 
-    public CreateMessageCommand(UUID id, UUID idFriend, String text) {
+    public CreateMessageCommand(UUID id, String text) {
         this.id = id;
-        this.idFriend = idFriend;
         this.text = text;
     }
 
     public UUID getId() {
         return id;
-    }
-
-    public UUID getIdFriend() {
-        return idFriend;
     }
 
     public String getText() {
--- message-repository/src/main/java/sigma/software/messagerepository/event/MessageCreatedEvent.java
+++ message-repository/src/main/java/sigma/software/messagerepository/event/MessageCreatedEvent.java
@@ -4,21 +4,15 @@ import java.util.UUID;
 
 public class MessageCreatedEvent {
     private final UUID id;
-    private final UUID idFriend;
     private final String text;
 
-    public MessageCreatedEvent(UUID id, UUID idFriend, String text) {
+    public MessageCreatedEvent(UUID id, String text) {
         this.id = id;
-        this.idFriend = idFriend;
         this.text = text;
     }
 
     public UUID getId() {
         return id;
-    }
-
-    public UUID getIdFriend() {
-        return idFriend;
     }
 
     public String getText() {
