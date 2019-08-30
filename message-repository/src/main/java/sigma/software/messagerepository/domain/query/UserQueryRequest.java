cab9409b404cfe4457ca12cf5bc4ed93ebb7a706
Maksim Kostromin <daggerok@gmail.com>
2019-08-30 18:52:36 +0300

    #13 | include shit (C) whatthecommit.com

--- message-repository/src/main/java/sigma/software/messagerepository/domain/query/UserQueryRequest.java
+++ message-repository/src/main/java/sigma/software/messagerepository/domain/query/UserQueryRequest.java
@@ -2,6 +2,6 @@ package sigma.software.messagerepository.domain.query;
 
 import java.util.UUID;
 
-public interface UserQueryRequest {
+public interface UserQueryRequest extends QueryRequest {
     UUID getAggregateId();
 }
