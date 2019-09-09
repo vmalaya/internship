e7ad3e34bdff2500f27801a809fab6c8054227b6
Valentyna Mala <valentinkamalaya@gmail.com>
2019-09-09 16:38:33 +0300

    Add xvfb for chrome on Travis CI. #21

--- selenide/src/test/java/GoogleTest.java
+++ selenide/src/test/java/GoogleTest.java
@@ -1,21 +1,30 @@
-import com.codeborne.selenide.CollectionCondition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.cssClass;
-import static com.codeborne.selenide.Condition.textCaseSensitive;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GoogleTest {
+
     @Test
     void should_find_and_open_page_then_open_file_and_assure_that_current_file_was_needed() {
         open("https://google.com");
+
         $(By.name("q")).setValue("daggerok selenide e2e tests").pressEnter();
-        $$("#rso .g").shouldHave(CollectionCondition.sizeGreaterThan(2))
-                     .get(1)
-                     .click();
-        $$(byText("README.md")).filterBy(cssClass("js-navigation-open")).first().click();
-        $("#readme").shouldHave(textCaseSensitive("E2E testing"));
+
+        $(byText("daggerok/testcontainers-selenide-example: E2E testing ... - GitHub"))
+                .shouldBe(exist)
+                .shouldBe(visible)
+                .click();
+
+        $$(byText("README.md"))
+                .filterBy(cssClass("js-navigation-open"))
+                .first()
+                .click();
+
+        $("#readme")
+                .shouldHave(textCaseSensitive("E2E testing"))
+                .shouldHave(textCaseSensitive("GitHub: selenide-examples"));
     }
 }
