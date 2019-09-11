2e7656c9f9cb7e6d794742b22cf94bbe08b9d711
Valentyna Mala <valentinkamalaya@gmail.com>
2019-09-11 12:33:50 +0300

    MT-1: Add tests to QueryGatewayTest. #25

--- messenger/src/test/java/sigma/software/messagerepository/domain/service/gateway/repository/UserRepositoryMockTest.java
+++ messenger/src/test/java/sigma/software/messagerepository/domain/service/gateway/repository/UserRepositoryMockTest.java
@@ -18,7 +18,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
