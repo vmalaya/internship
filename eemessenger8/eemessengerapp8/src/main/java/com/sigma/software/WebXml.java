bbaa014e206f60c230df75bcdb37793e2e9734c6
Valentyna Mala <valentinkamalaya@gmail.com>
2019-10-22 16:32:22 +0300

    BM-1: Update README.md. #48

---
+++ eemessenger8/eemessengerapp8/src/main/java/com/sigma/software/WebXml.java
@@ -0,0 +1,29 @@
+package com.sigma.software;
+
+import org.apache.struts2.dispatcher.filter.StrutsPrepareAndExecuteFilter;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+@WebFilter(
+        urlPatterns = "/*",
+        displayName = "JBoss EAP CDI Struts 2",
+        initParams = {
+                @WebInitParam(
+                        description = "Set action suffix as *Page",
+                        name = "struts.convention.action.suffix",
+                        value = "Page"
+                ),
+                @WebInitParam(
+                        description = "Set default package name as pages",
+                        name = "struts.convention.package.locators",
+                        value = "pages"
+                ),
+                @WebInitParam(
+                        description = "Set dev mode",
+                        name = "struts.devMode",
+                        value = "true"
+                )
+        }
+)
+public class WebXml extends StrutsPrepareAndExecuteFilter { }
---
+++ eemessenger8/eemessengerapp8/src/main/webapp/WEB-INF/content/index.jsp
@@ -0,0 +1,16 @@
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>EE Messenger</title>
+</head>
+<body>
+<h1>Hello, It is EE messenger implementation!</h1>
+
+    <s:form action="saveUsername" method="post" namespace="/">
+        <s:textfield name="username" label="username"/>
+        <s:submit value="Press me to save username"/>
+    </s:form>
+    <s:property value="usernames"/>
+</body>
+</html>
