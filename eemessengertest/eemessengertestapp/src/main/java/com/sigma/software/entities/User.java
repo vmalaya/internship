ea4379f053d4803f198b2072adf05291fa11f4dc
Valentyna Mala <valentinkamalaya@gmail.com>
2019-10-30 18:49:05 +0200

    C-1: Drafted version project structure with profiles. #54

---
+++ eemessengertest/eemessengertestapp/src/main/java/com/sigma/software/entities/User.java
@@ -0,0 +1,46 @@
+package com.sigma.software.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public User() {
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
